96cbec2f319f8c96eafe58b1e0c1f76c344de4e9
WANG KUANG WEI <kuangwei04@gmail.com>
2025-06-15 19:04:57 +0800

    Implemented Factory Pattern for Users and Database Models

--- public_service_locator/services/models.py
+++ public_service_locator/services/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+# factory pattern for users
+class UserFactory:
+  def create_user(self, role, username, password):
+    user = User.objects.create_user(username=username, password=password)
+    if role == "admin":
+      user.is_staff = True
+    user.save()
+    return user
+  
+# database models
+class Service(models.Model):
+  name = models.CharField(max_length=100)
+  category = models.CharField(max_length=50)
+  location = models.CharField(max_length=100)
+  approved = models.BooleanField(default=False) # for admin approval
