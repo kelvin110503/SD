85a9af95e5882b11a7262264b8f01f9d51e9d315
WANG KUANG WEI <kuangwei04@gmail.com>
2025-06-15 19:06:18 +0800

    Create Basic Login/Registration

--- public_service_locator/services/views.py
+++ public_service_locator/services/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
 
-# Create your views here.
+# basic login/registration
+def register(request):
+  if request.method == 'POST':
+    role = request.POST['role'] # the roles are: 'general', 'provider', 'admin'
+    user = UserFactory().create_user(role, request.POST['username'], request.POST['password'])
+    login(request, user)
